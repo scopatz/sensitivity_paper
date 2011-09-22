788dc4ead6516a988381403abb5cbe160351e1de
Anthony Scopatz <ascopatz@enthought.com>
2011-09-22 15:32:40 -0500

    Paper submitted.

--- make_paper.sh
+++ make_paper.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 latex sensitivity_paper.tex 
-#bibtex sensitivity_paper.aux 
+bibtex sensitivity_paper.aux 
 latex sensitivity_paper.tex 
 latex sensitivity_paper.tex 
 dvipdf sensitivity_paper.dvi
